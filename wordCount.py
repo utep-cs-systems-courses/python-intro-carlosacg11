3e48fbf71a01f117aa9875b8afd9a05f98f7cc84
carlosacg11 <55123698+carlosacg11@users.noreply.github.com>
2020-09-02 14:58:43 -0600

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -4,24 +4,7 @@
 Created on Wed Sep  2 12:00:19 2020
 
 @author: CarlosCardenas
-"""
-
-# FILE_NAME = 'speech.txt'
-
-
-# file=open(FILE_NAME,"r+")
-# wordcount={}
-
-# for word in file.read().split():
-#     if word not in wordcount:
-#         wordcount[word] = 1
-#     else:
-#         wordcount[word] += 1
-#     with open('output.txt', 'w') as f:
-#         for k,v in wordcount.items():
-#             print(k, v, file=f)
-#    # print (k, v)
-    
+"""    
 
 def countWords(rFile,wFile):
     file=open(rFile,"r+")
